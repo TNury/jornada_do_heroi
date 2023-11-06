29b9ca731e252434ba60563f6f073df8d9011094
Yuri Souza <yurdesou@gmail.com>
2023-11-06 14:48:30 -0300

    feat: Update author styles on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,15 +17,17 @@ const Home = async () => {
   return (
     <main className='relative flex min-h-screen w-full justify-center px-8 py-8 md:py-16'>
       <div className='flex w-full max-w-screen-lg flex-col gap-4'>
-        <div className='flex items-end justify-between'>
-          <h1 className='mr-auto text-4xl text-orange-400 md:text-6xl'>
+        <div className='flex flex-col md:flex-row md:items-end md:justify-between'>
+          <h1 className='text-4xl text-orange-400 md:text-6xl'>
             A hero's journey
           </h1>
-          <p className='text-2xl text-orange-400'>
+          <p className='text-xl md:text-2xl text-orange-400'>
             Made by{' '}
             <a
               href='https://www.linkedin.com/in/yuri-souza-b26bb3180/'
-              className='underline underline-offset-4 hover:opacity-50'>
+              className='border-b-2 border-orange-400 hover:opacity-50'
+              target="_blank"
+              >
               Yuri Souza
             </a>
           </p>
